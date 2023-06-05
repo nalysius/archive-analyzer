d9a306f8ce6a454b9e0bb8de68bba5dd91e2e7d1
Nalysius <anthony.bocci@protonmail.com>
2023-06-05 16:36:04 +0200

    Print central directory data
    When central directory will be detected, it will be possible to view its
    data

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ fn main() {
     let zip_file_result = reader::ZipFileReader::read(&mut file);
 
     println!("\n\n\n\n\n\n\n");
+    println!("Files stored in archive");
 
     if let Ok(zip_file) = zip_file_result {
         for stored_file in zip_file.stored_files {
@@ -28,19 +29,43 @@ fn main() {
             println!("\tCompressed size: {}", stored_file.local_file_header.compressed_size);
             println!("\tUncompressed size: {}", stored_file.local_file_header.uncompressed_size);
             println!("\tFilename: {}", stored_file.local_file_header.filename);
-            println!("\tFound in archive: {}", stored_file.found_in_archive);
             println!("\tFound in central directory: {}", stored_file.found_in_central_directory);
-            if stored_file.found_in_archive {
-                println!("\tPosition in archive: {}", stored_file.position_in_archive.unwrap());
-                println!("\tOffset from start of archive: {}", stored_file.offset_in_archive.or(Some(0)).unwrap());
-            }
-            if stored_file.found_in_central_directory {
-                println!("\tPosition in central directory: {}", stored_file.position_in_central_directory.unwrap());
-                println!("\tOffset from central directory: {}", stored_file.offset_from_central_directory.unwrap())
-            }
-            
+            println!("\tPosition in archive: {}", stored_file.position);
+            println!("\tOffset from start of archive: {}", stored_file.offset_in_archive);
+
             println!("\n")
         }
+
+        println!("Central directory");
+        if zip_file.central_directory.is_none() {
+            println!("\t No central directory found");
+        } else {
+            let central_directory = zip_file.central_directory.unwrap();
+
+            println!("\tHas a digital signature: {}", central_directory.digital_signature.is_some());
+            println!("\tNumber of central directory records on this disk: {}", central_directory.end_of_central_directory_record.central_directory_records_number_on_disk);
+            println!("\tTotal number of central directory records: {}", central_directory.end_of_central_directory_record.central_directory_records_total_number);
+            println!("\tSize of central directory: {}", central_directory.end_of_central_directory_record.central_directory_size);
+            println!("\tNumber of disks: {}", central_directory.end_of_central_directory_record.disk_number);
+            println!("\tDisk on which starts the central directory: {}", central_directory.end_of_central_directory_record.disk_start_central_directory);
+            println!("\tOffset of the central directory, relative to the start of archive: {}", central_directory.end_of_central_directory_record.offset_start_central_directory);
+
+            for central_directory_file_headers in central_directory.file_headers {
+                println!("\tCompressed size: {}", central_directory_file_headers.compressed_size);
+                println!("\tCompression method: {}", central_directory_file_headers.compression_method);
+                println!("\tCRC32: {}", central_directory_file_headers.crc32);
+                println!("\tDisk where the archive starts: {}", central_directory_file_headers.disk_start);
+                println!("\tExternal file attributes: {}", central_directory_file_headers.external_file_attributes);
+                println!("\tInternal file attributes: {}", central_directory_file_headers.internal_file_attributes);
+                println!("\tFile last modification time: {}", central_directory_file_headers.file_last_modification_time);
+                println!("\tFile last modification date: {}", central_directory_file_headers.file_last_modification_date);
+                println!("\tGeneral purpose flag: {}", central_directory_file_headers.general_purpose_flag);
+                //println!("\tExtra field: {}", central_directory_file_headers.extra_field);
+                println!("\tFile comment: {}", central_directory_file_headers.file_comment);
+                println!("\tFilename: {}", central_directory_file_headers.filename);
+            }
+        }
+
 
     }
     println!("\n\n\n\n\n\n\n");
